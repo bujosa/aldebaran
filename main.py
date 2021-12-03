45b10d91813114e970b973cb2e4d88d90509cc95
bujosa <davidbujosa@gmail.com>
2021-12-03 05:22:57 +0100

    Fix logic

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask.helpers import flash
 from google.cloud import pubsub_v1
 from flask import Flask, render_template, redirect, url_for
 from extract_functions.meli import main
-import datetime
 from webforms import FormMeli
 
 
@@ -21,17 +20,17 @@ app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 @app.route('/')
 def index():
-    return render_template('index.html', time=datetime.today().strftime('%Y-%m-%d'))
+    return render_template('index.html')
 
 @app.route('/form-rd', methods=['GET', 'POST'])
 def meli():
     form = FormMeli()
     if form.validate_on_submit():
-        if form.secret != os.environ['SECRET_KEY']:
+        if form.secret.data != os.environ['SECRET_KEY']:
             flash("Wrong Secret - Try Again!", "error")
         else:
             flash("Scrawling... Data will be available in a few hours")
-            main(int(form.days))
+            main(int(form.days.data))
             flash("Finish Scraping")
             return redirect(url_for('index'))            
     return render_template('meli.html', form=form)    
