90eb62299e969ef236aa51519de56e94b174d664
bujosa <davidbujosa@gmail.com>
2021-12-04 04:28:11 +0100

    Add picture shared

---
+++ shared/picture/picture.py
@@ -0,0 +1,13 @@
+# This function is used to get pictures from the gallery and get the number of pictures
+def get_gallery_pictures(soup):
+    pictures = []
+    try:
+        gallery_pictures = soup.find("div", class_="ui-pdp-gallery__column").find_all("span", class_="ui-pdp-gallery__wrapper")
+        for picture in gallery_pictures:
+            pictures.append(picture.find("img", class_="ui-pdp-image").get("data-src").replace("R.jpg", "F.jpg").replace("O.jpg", "F.jpg"))
+        
+        if len(pictures) > 5:
+            return pictures[0:4], len(pictures)
+        return pictures, len(pictures)
+    except:
+        return [], len(pictures)
