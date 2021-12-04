49bb8e0d2ba42b6364cc2b45fc7274f3e51ce09c
bujosa <davidbujosa@gmail.com>
2021-12-04 04:19:03 +0100

    Move seller to seller root

--- shared/utilities.py
+++ shared/utilities.py
@@ -153,20 +153,6 @@ def get_gallery_pictures(soup):
 def get_key(key):
     return fields[key]
 
-def get_seller(soup):
-    try:
-        seller = soup.find("h3", class_="ui-pdp-color--BLACK ui-pdp-size--LARGE ui-pdp-family--REGULAR").text
-        return seller
-    except:
-        return ''
-
-def get_seller_type(soup):
-    try:
-        sellerType = soup.find("p", class_="ui-pdp-color--GRAY ui-pdp-family--REGULAR ui-vip-profile-info__subtitle").text
-        return sellerType
-    except:
-        return 'Particular'
-
 def get_model(dict, title, brand):
   if title == None:
     return None
