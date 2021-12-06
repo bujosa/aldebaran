27019abe4696f994e5f76927f692547d85ef95ed
bujosa <davidbujosa@gmail.com>
2021-12-06 18:30:54 +0100

    Add meli cop

---
+++ database/mongo_cop.py
@@ -0,0 +1,14 @@
+import pymongo
+from datetime import datetime
+import os
+
+#Vehicle data manager class
+class VehicleDataManagerCop():
+    def __init__(self): 
+            self.connection = pymongo.MongoClient(os.environ['MONGO_DB_URI'])
+            db = self.connection['MELI_COP_V3']
+            collection_name=datetime.today().strftime('%Y-%m-%d')
+            self.collection = db[collection_name]
+
+    def addCar(self, vehicleObject):
+        self.collection.insert_one(vehicleObject)
