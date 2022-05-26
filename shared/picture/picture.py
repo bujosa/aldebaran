fd18db7841e7554f60f12db35d23ef5433a7070b
David Bujosa <davidbujosa@gmail.com>
2022-05-26 03:43:11 +0200

    fix: picture logic

--- shared/picture/picture.py
+++ shared/picture/picture.py
@@ -1,12 +1,22 @@
 # This function is used to get pictures from the gallery and get the number of pictures
 def get_gallery_pictures(soup):
     pictures = []
-    try:
-        gallery_pictures = soup.find("div", class_="ui-pdp-gallery__column").find_all("span", class_="ui-pdp-gallery__wrapper")
+
+    # try to get the pictures 
+    try:                                                                
+        gallery_pictures = soup.find("div", class_="ui-pdp-gallery__column").find_all("figure", class_="ui-pdp-gallery__figure")
+
         for picture in gallery_pictures:
-            pictures.append(picture.find("img", class_="ui-pdp-image").get("src").replace("R.jpg", "F.jpg").replace("O.jpg", "F.jpg"))
+            picture_in_gallery = picture.find("img", class_="ui-pdp-image").get("src").replace("R.jpg", "F.jpg").replace("O.jpg", "F.jpg").replace("O.webp", "F.jpg").replace("R.webp", "F.jpg")
+            
+            if picture == None:
+                continue
+
+            pictures.append(picture_in_gallery) 
+
         if len(pictures) > 5:
             return pictures[0:4], len(pictures)
+
         return pictures, len(pictures)
     except:
         return [], len(pictures)
